b39ebe3415a4fa2937c8620f53f531342f89b211
Sujin Lee <susin1227@naver.com>
2017-01-16 11:05:08 +0900

    20170116  quiz2 내가 한거

---
+++ HelloWorld/src/com/test/quiz2/Quiz2.java
@@ -0,0 +1,30 @@
+/*
+package com.test.quiz2;
+import java.util.Scanner;
+public class Quiz2{
+		public static void main(String[]args) {
+			int	KeyCode = System.in.read();
+			if (KeyCode == 49){
+			System.out.println("*\n**\n***\n****\n*****");
+			}else if (KeyCode == 52){
+			System.out.println("*****\n****\n***\n**\n*");	
+			}else if (KeyCode == 51){
+				System.out.println("*****\n ****\n  ***\n   **\n    *");
+			}else if (KeyCode == 50){
+				System.out.println("    *\n   **\n  ***\n ****\n*****");
+			}
+		}
+			
+}
+*/
+
+package com.test.quiz2;
+public class Quiz2{
+	public static void main(String[]args) {
+	int start1, start2, inc1, inc2 ;
+	}
+}
+
+
+
+
